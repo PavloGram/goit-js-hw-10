ae64b3ce9210529723d34ee51aca6ffd0f53c050
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-03-24 19:38:12 +0200

    df

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const input = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
 const info = document.querySelector('.country-info');
 
-const searchParams = new URLSearchParams({});
+
 
 input.addEventListener('input', debounce(inputCountriues, DEBOUNCE_DELAY));
 
@@ -16,33 +16,33 @@ function inputCountriues(e) {
   const input = e.target.value.trim();
   const url = `https://restcountries.com/v3.1/name/${input}?fields=name,capital,population,flags,languages`;
   fetchCountries(url)
-    .then(r => {
-      if (!r.ok) {
+    .then(response => {
+      if (!response.ok) {
         cleaeMarcup();
         return Notiflix.Notify.failure(
           'Oops, there is no country with that name'
         );
       }
-      return r.json();
+      return response.json();
     })
-    .then(c => {
-      return marcupList(c);
+    .then(forMarcup => {
+      return marcupList(forMarcup);
     })
     .catch(er => {
       Notiflix.Notify.failure('Oops, there is no country with that name');
     });
 }
 
-function marcupList(v) {
-  if (v.length > 10) {
+function marcupList(element) {
+  if (element.length > 10) {
     cleaeMarcup();
     return Notiflix.Notify.info(
       'Too many matches found. Please enter a more specific name.'
     );
   }
-  if (v.length > 2 && v.length < 10) {
+  if (element.length > 2 && element.length < 10) {
     cleaeMarcup();
-    const marcup = v
+    const marcup = element
       .map(e => {
         return `<li class="country-list__item" ><img src="${e.flags.svg}" width = 30 height = 20><p>${e.name.official}</p></li>`;
       })
@@ -50,19 +50,19 @@ function marcupList(v) {
     list.innerHTML = marcup;
   } else {
     cleaeMarcup();
-    const marcup = v
+    const marcup = element
       .map(e => {
         return `<ul class="country-info__list">
 <li class="country-info__item" ><img src="${
           e.flags.svg
         }" width = 40 height = 30><h1>${e.name.official}</h1> </li>
-<li><p class="country-info__text"><span class="country-info__item">Capital:</span> ${
+<li><p class="country-info__text"><span class="country-info__item">Capital:</span>${
           e.capital
         }</p></li>
-<li><p class="country-info__text"><span class="country-info__item">Population: </span>${
+<li><p class="country-info__text"><span class="country-info__item">Population:</span>${
           e.population
         }</p></li>
-<li><p class="country-info__text"><span class="country-info__item">Languages: </span> ${Object.values(
+<li><p class="country-info__text"><span class="country-info__item">Languages:</span> ${Object.values(
           e.languages
         ).join(', ')}</p> </li>
 </ul>`;

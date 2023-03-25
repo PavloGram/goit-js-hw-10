585ab2762354245d02ab81547d29831983c7d1f1
PavloGram <113470072+PavloGram@users.noreply.github.com>
2023-03-25 18:36:46 +0200

    pr

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,14 @@
-export function fetchCountries(url) {
-  return fetch(url);
+const BASIC_URL = 'https://restcountries.com/v3.1/name/';
+const searchParams = new URLSearchParams({
+  fields: 'name,capital,population,flags,languages',
+});
+
+export function fetchCountries(input) {
+  const url = `${BASIC_URL}${input}?${searchParams}`;
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
--- src/index.js
+++ src/index.js
@@ -11,18 +11,13 @@ const infoEl = document.querySelector('.country-info');
 inputEl.addEventListener('input', debounce(inputCountriues, DEBOUNCE_DELAY));
 
 function inputCountriues(e) {
-  const inputValue = e.target.value.trim();
-  const url = `https://restcountries.com/v3.1/name/${inputValue}?fields=name,capital,population,flags,languages`;
-  if (inputValue === '') {
-    return cleaeMarcup();
+  cleaeMarcup();
+  const input = e.target.value.trim();
+
+  if (input === '') {
+    return;
   }
-  fetchCountries(url)
-    .then(response => {
-      if (!response.ok) {
-         throw new Error(response.status)
-      }
-      return response.json();
-    })
+  fetchCountries(input)
     .then(forMarcup => {
       return marcupList(forMarcup);
     })
@@ -33,43 +28,47 @@ function inputCountriues(e) {
 
 function marcupList(element) {
   if (element.length > 10) {
-    cleaeMarcup();
     return Notiflix.Notify.info(
       'Too many matches found. Please enter a more specific name.'
     );
   }
   if (element.length > 2 && element.length < 10) {
-    cleaeMarcup();
-    const marcup = element
-      .map(e => {
-        return `<li class="country-list__item" ><img src="${e.flags.svg}" width = 30 height = 20><p>${e.name.official}</p></li>`;
-      })
-      .join('');
-    listEl.innerHTML = marcup;
+    countriesListMarcup(element);
   } else {
-    cleaeMarcup();
-    const marcup = element
-      .map(e => {
-        return `<ul class="country-info__list">
-<li class="country-info__item" ><img src="${
-          e.flags.svg
-        }" width = 40 height = 30><h1>${e.name.official}</h1> </li>
-<li><p class="country-info__text"><span class="country-info__item">Capital:</span>${
-          e.capital
-        }</p></li>
-<li><p class="country-info__text"><span class="country-info__item">Population:</span>${
-          e.population
-        }</p></li>
-<li><p class="country-info__text"><span class="country-info__item">Languages:</span> ${Object.values(
-          e.languages
-        ).join(', ')}</p> </li>
-</ul>`;
-      })
-      .join('');
-    infoEl.innerHTML = marcup;
+    countriInfoMarcup(element);
   }
 }
 
+function countriesListMarcup(element) {
+  const marcup = element
+    .map(e => {
+      return `<li class="country-list__item" ><img src="${e.flags.svg}" width = 30 height = 20><p>${e.name.official}</p></li>`;
+    })
+    .join('');
+  listEl.innerHTML = marcup;
+}
+
+function countriInfoMarcup(element) {
+  const marcup = element
+    .map(e => {
+      return `<ul class="country-info__list">
+<li class="country-info__item" ><img src="${
+        e.flags.svg
+      }" width = 40 height = 30><h1>${e.name.official}</h1> </li>
+<li><p class="country-info__text"><span class="country-info__item">Capital:</span>${
+        e.capital
+      }</p></li>
+<li><p class="country-info__text"><span class="country-info__item">Population:</span>${
+        e.population
+      }</p></li>
+<li><p class="country-info__text"><span class="country-info__item">Languages:</span> ${Object.values(
+        e.languages
+      ).join(', ')}</p> </li>
+</ul>`;
+    })
+    .join('');
+  infoEl.innerHTML = marcup;
+}
 function cleaeMarcup() {
   listEl.innerHTML = '';
   infoEl.innerHTML = '';
